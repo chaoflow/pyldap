947416045a72be5dd31620ef2b51082e6cb45e60
Alexander Pilz <pilz@syslab.com>
2013-07-09 16:52:49 +0200

    Preparing release 0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.6.dev0'
+version = '0.6'
 shortdesc = '\
 A more pythonic API to python-ldap, possibly to be merged into python-ldap.'
 
